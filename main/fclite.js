176de6bea9db77ca7930128673088e27eef4718b
柳神 <3162475700@qq.com>
2024-09-12 20:44:47 +0800

    😬小小更新，修改一下页脚链接地址

--- main/fclite.js
+++ main/fclite.js
@@ -60,7 +60,7 @@ function initialize_fc_lite() {
         const stats = data.statistical_data;
         statsContainer.innerHTML = `
             <div>Powered by: <a href="https://github.com/willow-god/Friend-Circle-Lite" target="_blank">FriendCircleLite</a><br></div>
-            <div>Designed By: <a href="https://www.qyliu.top/" target="_blank">LiuShen</a><br></div>
+            <div>Designed By: <a href="https://www.liushen.fun/" target="_blank">LiuShen</a><br></div>
             <div>订阅:${stats.friends_num}   活跃:${stats.active_num}   总文章数:${stats.article_num}<br></div>
             <div>更新时间:${stats.last_updated_time}</div>
         `;
