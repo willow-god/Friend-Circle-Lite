f2c028c837d1a7c9d2dfd0e4da9803ffe33af0fd
LiuShen <3162475700@qq.com>
2025-05-16 22:03:11 +0800

    👨🏼‍🦰移除不正确的Brotli压缩方式(问题严重，建议更新！)

--- friend_circle_lite/get_info.py
+++ friend_circle_lite/get_info.py
@@ -26,9 +26,9 @@ HEADERS_XML = {
         "Chrome/123.0.0.0 Safari/537.36 "
         "(Friend-Circle-Lite/1.0; +https://github.com/willow-god/Friend-Circle-Lite)"
     ),
-    "Accept": "application/rss+xml, application/xml;q=0.9, */*;q=0.8",
+    "Accept": "application/atom+xml, application/rss+xml, application/xml;q=0.9, */*;q=0.8",
     "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Encoding": "gzip, deflate",
     "Connection": "keep-alive",
     "X-Friend-Circle": "1.0"
 }
